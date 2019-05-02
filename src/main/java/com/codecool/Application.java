fff29047ae7204a1039bb897e49ad500bf16fa0a
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-05-02 12:16:59 +0200

    Add main function to run program

--- src/main/java/com/codecool/Application.java
+++ src/main/java/com/codecool/Application.java
@@ -1,4 +1,10 @@
 package com.codecool;
 
+import com.codecool.Controller.Controller;
+
 public class Application {
+    public static void main(String[] args) {
+        Controller controller = new Controller(args);
+        controller.start();
+    }
 }
