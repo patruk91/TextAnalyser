92429be79994faa030f2084358d1a0c29b032ae7
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-05-02 09:47:35 +0200

    Add FileReader class, and method get file content

---
+++ src/main/java/com/codecool/FileHandler/FileReader.java
@@ -0,0 +1,27 @@
+package com.codecool.FileHandler;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileReader {
+
+    protected String getFileContent(String fileName) {
+        String data = "";
+        try {
+            data = Files.readString(Paths.get(fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
+
+//        StringBuilder sb = new StringBuilder();
+//        try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(fileName))) {
+//            String line;
+//            while ((line = bufferedReader.readLine()) != null) {
+//                sb.append(line).append("\n");
+//            }
+//        }
+//        return sb.toString();
+    }
+}
