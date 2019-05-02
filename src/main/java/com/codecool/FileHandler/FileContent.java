961581a3cee5c27f69eaaffdfeae37dcd533dc6b
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-05-02 07:50:24 +0200

    Implement methods: charIterator, wordIterator

--- src/main/java/com/codecool/FileHandler/FileContent.java
+++ src/main/java/com/codecool/FileHandler/FileContent.java
@@ -1,4 +1,26 @@
 package com.codecool.FileHandler;
 
+import com.codecool.Iterator.CharIterator;
+import com.codecool.Iterator.WordIterator;
+
+import java.util.Iterator;
+
 public class FileContent implements IterableText {
+
+    private String fileName;
+
+    public FileContent(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public Iterator<String> charIterator() {
+        return new CharIterator(fileName);
+    }
+
+    @Override
+    public Iterator<String> wordIterator() {
+        return new WordIterator(fileName);
+    }
+
 }
