3495270de810d7e299a92f76d2d6a6304ea824f9
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-05-02 09:45:55 +0200

    Implement iterator methods, and add characters from file

--- src/main/java/com/codecool/Iterator/CharIterator.java
+++ src/main/java/com/codecool/Iterator/CharIterator.java
@@ -1,6 +1,42 @@
 package com.codecool.Iterator;
 
-import java.util.Iterator;
+import com.codecool.FileHandler.FileReader;
 
-public class CharIterator implements Iterator<String> {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class CharIterator extends FileReader implements Iterator<String>{
+    private String fileContent;
+    private int index = 0;
+    private List<String> charactersList = new ArrayList<>();
+
+    public CharIterator(String fileContent){
+        this.fileContent = fileContent;
+        addCharsFromFile();
+    }
+
+    private void addCharsFromFile(){
+        String content = getFileContent(fileContent);
+        for (Character character : content.toCharArray()) {
+            if (Character.isLetterOrDigit(character)) {
+                charactersList.add(character.toString().toLowerCase());
+            }
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < charactersList.size();
+    }
+
+    @Override
+    public String next() {
+        if (!this.hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return charactersList.get(index++);
+    }
+
 }
