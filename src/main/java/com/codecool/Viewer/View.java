2ac8e93affe2fc1086c7a2660d175eee68e30a61
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-05-02 14:31:45 +0200

    Implement methods to display: fileName, Error

--- src/main/java/com/codecool/Viewer/View.java
+++ src/main/java/com/codecool/Viewer/View.java
@@ -1,4 +1,12 @@
 package com.codecool.Viewer;
 
 public class View {
+
+    public void displayFileName(String filename) {
+        System.out.println("File name: " + filename);
+    }
+
+    public void displayError(String message) {
+        System.out.println("ERROR: " + message);
+    }
 }
