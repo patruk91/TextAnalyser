7f55b3bce3f386e4c11b8ed48dceb022e73b2d81
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-05-02 14:31:18 +0200

    Add start method to handle control program flow

--- src/main/java/com/codecool/Controller/Controller.java
+++ src/main/java/com/codecool/Controller/Controller.java
@@ -1,4 +1,32 @@
 package com.codecool.Controller;
 
+import com.codecool.FileAnalysis.StatisticalAnalysis;
+import com.codecool.FileHandler.FileContent;
+import com.codecool.FileHandler.IterableText;
+import com.codecool.Viewer.View;
+
 public class Controller {
+    private String[] args;
+    private View view = new View();
+
+    public Controller(String[] args) {
+        this.args = args;
+    }
+
+    public void start() {
+        if (args.length == 0) {
+            view.displayError("No file provided!");
+        } else {
+            for (String arg : args) {
+                IterableText iterableText = new FileContent(arg);
+                showResults(arg, iterableText);
+            }
+        }
+    }
+
+    private void showResults(String arg, IterableText iterableText) {
+        view.displayFileName(arg);
+    }
+
+
 }
