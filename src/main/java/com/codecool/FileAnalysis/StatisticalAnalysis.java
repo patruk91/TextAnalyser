5ebcd3243bfdc4c9cb9a078c2ba76ad9c85de514
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-05-02 12:10:56 +0200

    Implement countOf method, to calculate sum of all occurences

--- src/main/java/com/codecool/FileAnalysis/StatisticalAnalysis.java
+++ src/main/java/com/codecool/FileAnalysis/StatisticalAnalysis.java
@@ -1,4 +1,25 @@
 package com.codecool.FileAnalysis;
 
+import java.util.*;
+
 public class StatisticalAnalysis {
+    private Iterator<String> iterator;
+
+    public StatisticalAnalysis(Iterator<String> iterator) {
+        this.iterator = iterator;
+    }
+
+    public int countOf(String... elements) {
+        int occurrences = 0;
+        while (iterator.hasNext()) {
+            if (Arrays.asList(elements).contains(iterator.next())) {
+                occurrences++;
+            }
+        }
+        return occurrences;
+    }
+
+
+
+
 }
