66d52c1aac25057a0133c78c500f57e8da84dea6
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-05-02 09:55:47 +0200

    Implement iterator methods, add method: add words from file

--- src/main/java/com/codecool/Iterator/WordIterator.java
+++ src/main/java/com/codecool/Iterator/WordIterator.java
@@ -1,6 +1,33 @@
 package com.codecool.Iterator;
 
-import java.util.Iterator;
+import com.codecool.FileHandler.FileReader;
 
-public class WordIterator implements Iterator<String> {
+import java.util.*;
+
+public class WordIterator extends FileReader implements Iterator<String> {
+    private String fileContent;
+    private int index = 0;
+    private List<String> wordList = new ArrayList<>();
+
+    public WordIterator(String fileContent){
+        this.fileContent = fileContent;
+        addWordsFromFile();
+    }
+
+    private void addWordsFromFile() {
+        String content = getFileContent(fileContent);
+        wordList = Arrays.asList(content.trim().split("\\s+"));
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < wordList.size();
+    }
+
+    @Override
+    public String next() {
+        if (!this.hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return wordList.get(index++);    }
 }
