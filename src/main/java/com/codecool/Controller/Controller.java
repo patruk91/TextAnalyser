d7fdba1633099ef0f24703729271297a5abf65c6
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-05-03 07:51:39 +0200

    Implement methods fo show file name, count characters and words

--- src/main/java/com/codecool/Controller/Controller.java
+++ src/main/java/com/codecool/Controller/Controller.java
@@ -5,6 +5,12 @@ import com.codecool.FileHandler.FileContent;
 import com.codecool.FileHandler.IterableText;
 import com.codecool.Viewer.View;
 
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+
 public class Controller {
     private String[] args;
     private View view = new View();
@@ -14,6 +20,7 @@ public class Controller {
     }
 
     public void start() {
+        long startTime = System.currentTimeMillis();
         if (args.length == 0) {
             view.displayError("No file provided!");
         } else {
@@ -21,12 +28,35 @@ public class Controller {
                 IterableText iterableText = new FileContent(arg);
                 showResults(arg, iterableText);
             }
+            long endTime = System.currentTimeMillis();
+            showBenchmarkTime(startTime, endTime);
         }
     }
 
     private void showResults(String arg, IterableText iterableText) {
+        showFileName(arg);
+        showCountCharacters(iterableText);
+        showCountWords(iterableText);
+
+    }
+
+
+
+    private void showCountWords(IterableText iterableText) {
+        int wordListSize = new StatisticalAnalysis(iterableText.wordIterator()).size();
+        view.displayMessage("Word count: " + wordListSize);
+    }
+
+    private void showCountCharacters(IterableText iterableText) {
+        int charListSize = new StatisticalAnalysis(iterableText.charIterator()).size();
+        view.displayMessage("Char count: " + charListSize);
+    }
+
+    private void showFileName(String arg) {
         view.displayFileName(arg);
     }
+
+
 
 
 }
