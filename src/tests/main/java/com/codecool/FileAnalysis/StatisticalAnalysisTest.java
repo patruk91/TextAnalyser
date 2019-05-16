bcd40e2bc81ad3330dfc55fcaa145af3bf860927
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-05-16 08:07:37 +0200

    Add test to count words: love, hate, music

--- src/tests/main/java/com/codecool/FileAnalysis/StatisticalAnalysisTest.java
+++ src/tests/main/java/com/codecool/FileAnalysis/StatisticalAnalysisTest.java
@@ -1,20 +1,48 @@
 package com.codecool.FileAnalysis;
 
-import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import com.codecool.FileHandler.FileContent;
+import com.codecool.FileHandler.IterableText;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticalAnalysisTest {
 
+    @BeforeClass
+    IterableText setupFile() {
+        return new FileContent("test.txt");
+    }
+
+    @BeforeClass
+    StatisticalAnalysis setupCharacter() {
+        IterableText iterableText = setupFile();
+        return new StatisticalAnalysis(iterableText.charIterator());
+    }
+
+    @BeforeClass
+    StatisticalAnalysis setupWord() {
+        IterableText iterableText = setupFile();
+        return new StatisticalAnalysis(iterableText.wordIterator());
+    }
 
     @Test
-    void countOf() {
+    void testCountOfLove() {
+        StatisticalAnalysis word = setupWord();
+        assertEquals(1, word.countOf("love"), "Number of occurrences of the word: 'love': ");
+    }
 
+    @Test
+    void testCountOfHate() {
+        StatisticalAnalysis word = setupWord();
+        assertEquals(0, word.countOf("hate"), "Number of occurrences of the word: 'hate': ");
+    }
+
+    @Test
+    void testCountOfMusic() {
+        StatisticalAnalysis word = setupWord();
+        assertEquals(3, word.countOf("music"), "Number of occurrences of the word: 'music': ");
     }
 
     @Test
