cd046596ab022c493d4afd5d4758168e00efa252
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-05-16 07:10:12 +0200

    Add test folders to program and dependencies in pom

---
+++ src/tests/main/java/com/codecool/FileAnalysis/StatisticalAnalysisTest.java
@@ -0,0 +1,31 @@
+package com.codecool.FileAnalysis;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticalAnalysisTest {
+
+
+    @Test
+    void countOf() {
+
+    }
+
+    @Test
+    void dictionarySize() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void occurMoreThan() {
+    }
+}
